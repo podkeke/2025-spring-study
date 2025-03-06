c62c02135aac8cf98c397802d1d7cd7a97401ba6
노은서 <151901774+podkeke@users.noreply.github.com>
2025-03-07 04:48:20 +0900

    section7 finished

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.*;
+
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //고객 id(x)/저장하기위한 시스템아이디(o)
+
     private String name;
 
     public Long getId() {
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,9 +5,12 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+
+@Transactional
 
 public class MemberService {
 
@@ -17,7 +20,7 @@ public class MemberService {
     //->
     private final MemberRepository memberRepository;
 
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) {
        this.memberRepository = memberRepository;
     }
